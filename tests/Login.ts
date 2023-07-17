d8c194ffb016942b066e65697fdfa44fafad2b58
judelyn.cabalhao@lanexcorp.com <judelyn.cabalhao@lanexcorp.com>
2023-07-17 21:58:27 +0800

    Done

---
+++ tests/Login.ts
@@ -0,0 +1,94 @@
+import { test, expect } from '@playwright/test';
+
+test('Successful login', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await page.getByPlaceholder('メールアドレスを入力する').click();
+  await page.getByPlaceholder('メールアドレスを入力する').fill('shunwasaki144@gmail.com');
+  await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+  await page.getByPlaceholder('パスワードを入力する').fill('Test@123!');
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  page.waitForLoadState
+  await expect(page).toHaveURL(/.*dashboard/);
+  await page.close();
+});
+
+
+test('Invalid Email', async ({ page }) => {
+    await page.goto('https://dev.regalis.app/');
+   await expect(page.getByText ('メールアドレス')).toBeVisible();
+    await page.getByPlaceholder('メールアドレスを入力する').click();
+    await page.getByPlaceholder('メールアドレスを入力する').fill('test');
+    await expect (page.getByText('有効なEメールアドレスを入力してください', { exact: true })).toBeVisible();
+    await page.close();
+  });
+
+  
+test('Non-existing  Email', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+ await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await page.getByPlaceholder('メールアドレスを入力する').click();
+  await page.getByPlaceholder('メールアドレスを入力する').fill('shunwasaki@gmail.com');
+  await page.getByPlaceholder('パスワードを入力する').fill('Test@123!');
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect (page.getByText('無効なユーザー名', { exact: true })).toBeVisible();
+  await page.close();
+
+});
+
+  
+test('Incorrect password', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await page.getByPlaceholder('メールアドレスを入力する').click();
+  await page.getByPlaceholder('メールアドレスを入力する').fill('shunwasaki144@gmail.com');
+  await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+  await page.getByPlaceholder('パスワードを入力する').fill('Test123!');
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect (page.getByText('パスワードが正しくない', { exact: true })).toBeVisible();
+  await page.close();
+
+
+});
+
+test('Empty email field', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+  await page.getByPlaceholder('パスワードを入力する').fill('Test123!');
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect (page.getByText('有効なEメールアドレスを入力してください', { exact: true })).toBeVisible();
+  await page.close();
+
+
+});
+
+test('Empty login', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await page.getByPlaceholder('メールアドレスを入力する').click();
+  await page.getByPlaceholder('メールアドレスを入力する').fill('shunwasaki144@gmail.com');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect (page.getByText('パスワードが必要', { exact: true })).toBeVisible();
+  await page.close();
+
+});
+
+test.only('No input on fields', async ({ page }) => {
+  await page.goto('https://dev.regalis.app/');
+  await expect(page.getByText ('メールアドレス')).toBeVisible();
+  await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await expect (page.getByText('有効なEメールアドレスを入力してください', { exact: true })).toBeVisible();
+  await expect (page.getByText('パスワードが必要', { exact: true })).toBeVisible();
+  await page.close();
+
+});
+
+
+
+
+
+
