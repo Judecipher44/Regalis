5d365cb0aa155c0eb8cd0af708b999b78f1649e9
judelyn.cabalhao@lanexcorp.com <judelyn.cabalhao@lanexcorp.com>
2023-07-18 09:45:54 +0800

    Done

---
+++ tests/dashboard.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+
+test.only('search user name', async ({ page }) => {
+    await page.goto('https://dev.regalis.app/');
+    await expect(page.getByText ('メールアドレス')).toBeVisible();
+    await page.getByPlaceholder('メールアドレスを入力する').click();
+    await page.getByPlaceholder('メールアドレスを入力する').fill('shunwasaki144@gmail.com');
+    await expect (page.getByText('パスワード', { exact: true })).toBeVisible();
+    await page.getByPlaceholder('パスワードを入力する').fill('Test@123!');
+    await page.getByRole('button', { name: 'ログイン' }).click();
+    await expect(page).toHaveURL(/.*dashboard/);
+    await page.getByRole('heading', { name: 'Dashboard' }).click();
+    await page.close();
+
+    });
